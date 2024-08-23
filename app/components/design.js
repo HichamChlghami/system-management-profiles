da6a45e461f79a7e0c13f4fa29030386fe7629a8
Hicham <chlghamihicham@gmail.com>
2024-08-23 19:48:57 +0100

    ijhb

--- app/components/design.js
+++ app/components/design.js
@@ -46,20 +46,22 @@ function Design  ({
 
 // // this for show alert 
 
-const [isOnline, setIsOnline] = useState(navigator.onLine);
-  const [showAlert, setShowAlert] = useState(false);
+const [isOnline, setIsOnline] = useState(false);
+const [showAlert, setShowAlert] = useState(false);
 
-  useEffect(() => {
-    const updateOnlineStatus = () => {
-        if (typeof navigator !== 'undefined') {
-            setIsOnline(navigator.onLine);
-            setShowAlert(true);
-            if (navigator.onLine) {
-                setTimeout(() => setShowAlert(false), 5000);
-            }
-        }
-    };
+useEffect(() => {
+  const updateOnlineStatus = () => {
+    if (typeof navigator !== 'undefined') {
+      setIsOnline(navigator.onLine);
+      setShowAlert(true);
+      if (navigator.onLine) {
+        setTimeout(() => setShowAlert(false), 5000);
+      }
+    }
+  };
 
+  // Only run the following code in the browser
+  if (typeof window !== 'undefined') {
     window.addEventListener('online', updateOnlineStatus);
     window.addEventListener('offline', updateOnlineStatus);
 
@@ -67,13 +69,15 @@ const [isOnline, setIsOnline] = useState(navigator.onLine);
     updateOnlineStatus();
 
     return () => {
-        window.removeEventListener('online', updateOnlineStatus);
-        window.removeEventListener('offline', updateOnlineStatus);
+      window.removeEventListener('online', updateOnlineStatus);
+      window.removeEventListener('offline', updateOnlineStatus);
     };
+  }
 }, []);
-  const handleDismiss = () => {
-    setShowAlert(false);
-  };
+
+const handleDismiss = () => {
+  setShowAlert(false);
+};
 // // end for show alert
 
 
@@ -453,17 +457,16 @@ const truncateFileName = (fileName) => {
 <div className="convert" onDrop={handleDrop}onDragOver={handleDragOver}>
        <Navbar/> 
 
-<>
+       <>
       {showAlert && (
-        <div className='alert_section'
+        <div
+          className='alert_section'
           style={{
             backgroundColor: isOnline ? '#28a745' : '#e57373',
-           
           }}
         >
-          {isOnline ? 'Network connected You are now online' : 'Offline: Tasks will resume once connected'}
-         
-          <AiOutlineClose  className='alert_close'  onClick={handleDismiss} />
+          {isOnline ? 'Network connected. You are now online' : 'Offline: Tasks will resume once connected'}
+          <AiOutlineClose className='alert_close' onClick={handleDismiss} />
         </div>
       )}
     </>
