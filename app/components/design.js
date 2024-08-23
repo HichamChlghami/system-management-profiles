63bbee8535941044c5850d0162a50ca9a6c64468
Hicham <chlghamihicham@gmail.com>
2024-08-23 19:41:45 +0100

    poiuh

--- app/components/design.js
+++ app/components/design.js
@@ -46,34 +46,34 @@ function Design  ({
 
 // // this for show alert 
 
-// const [isOnline, setIsOnline] = useState(navigator.onLine);
-//   const [showAlert, setShowAlert] = useState(false);
+const [isOnline, setIsOnline] = useState(navigator.onLine);
+  const [showAlert, setShowAlert] = useState(false);
 
-//   useEffect(() => {
-//     const updateOnlineStatus = () => {
-//         if (typeof navigator !== 'undefined') {
-//             setIsOnline(navigator.onLine);
-//             setShowAlert(true);
-//             if (navigator.onLine) {
-//                 setTimeout(() => setShowAlert(false), 5000);
-//             }
-//         }
-//     };
+  useEffect(() => {
+    const updateOnlineStatus = () => {
+        if (typeof navigator !== 'undefined') {
+            setIsOnline(navigator.onLine);
+            setShowAlert(true);
+            if (navigator.onLine) {
+                setTimeout(() => setShowAlert(false), 5000);
+            }
+        }
+    };
 
-//     window.addEventListener('online', updateOnlineStatus);
-//     window.addEventListener('offline', updateOnlineStatus);
+    window.addEventListener('online', updateOnlineStatus);
+    window.addEventListener('offline', updateOnlineStatus);
 
-//     // Initial check
-//     updateOnlineStatus();
+    // Initial check
+    updateOnlineStatus();
 
-//     return () => {
-//         window.removeEventListener('online', updateOnlineStatus);
-//         window.removeEventListener('offline', updateOnlineStatus);
-//     };
-// }, []);
-//   const handleDismiss = () => {
-//     setShowAlert(false);
-//   };
+    return () => {
+        window.removeEventListener('online', updateOnlineStatus);
+        window.removeEventListener('offline', updateOnlineStatus);
+    };
+}, []);
+  const handleDismiss = () => {
+    setShowAlert(false);
+  };
 // // end for show alert
 
 
@@ -452,7 +452,7 @@ const truncateFileName = (fileName) => {
 
 <div className="convert" onDrop={handleDrop}onDragOver={handleDragOver}>
        <Navbar/> 
-{/* 
+
 <>
       {showAlert && (
         <div className='alert_section'
@@ -466,7 +466,7 @@ const truncateFileName = (fileName) => {
           <AiOutlineClose  className='alert_close'  onClick={handleDismiss} />
         </div>
       )}
-    </> */}
+    </>
 
 
 
