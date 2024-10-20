9aad318f5a0e8716c6832469158219a8d2165369
Hicham <121503241+HichamChlghami@users.noreply.github.com>
2024-10-20 14:26:42 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
-// next.config.js
-module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          has: [
-            {
-              type: 'host',
-              value: 'https://sitfile.com', // The non-www domain
-            },
-          ],
-          destination: 'https://www.sitfile.com', // Redirect to the www version
-          permanent: true, // This will issue a 301 redirect
-        },
-      ];
-    },
-  };
-  
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output:"export"
+    
+    }
+    
+    module.exports = nextConfig
