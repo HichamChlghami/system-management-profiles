8c96152a191de37f70026f7dcfa5e2013fc70721
Hicham <chlghamihicham@gmail.com>
2024-10-20 14:10:06 +0100

    filirrd

--- next.config.js
+++ next.config.js
@@ -1,30 +1,19 @@
-// // next.config.js
-// module.exports = {
-//     async redirects() {
-//       return [
-//         {
-//           source: '/:path*.html',
-//           destination: '/:path*',
-//           permanent: true,
-//         },
-//       ]
-//     },
-//   }
+// next.config.js
+module.exports = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          has: [
+            {
+              type: 'host',
+              value: 'sitfile.com', // The non-www domain
+            },
+          ],
+          destination: 'https://www.sitfile.com/', // Redirect to the www version
+          permanent: true, // This will issue a 301 redirect
+        },
+      ];
+    },
+  };
   
-
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output:"export"
-    
-    }
-    
-    module.exports = nextConfig
