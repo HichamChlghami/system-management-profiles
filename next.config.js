d31d15e37c0de5e4db08ca137da4b8bd3eb4fdaf
Hicham <121503241+HichamChlghami@users.noreply.github.com>
2024-10-20 14:22:14 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,10 +7,10 @@ module.exports = {
           has: [
             {
               type: 'host',
-              value: 'sitfile.com', // The non-www domain
+              value: 'https://sitfile.com', // The non-www domain
             },
           ],
-          destination: 'https://www.sitfile.com/', // Redirect to the www version
+          destination: 'https://www.sitfile.com', // Redirect to the www version
           permanent: true, // This will issue a 301 redirect
         },
       ];
