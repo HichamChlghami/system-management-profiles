bcc48c6c33d79c2d1b7b1c9461d5885b4d7e10cd
Hicham <121503241+HichamChlghami@users.noreply.github.com>
2024-10-20 14:43:41 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export"
-    
-    }
-    
-    module.exports = nextConfig
+  async redirects() {
+    return [
+      {
+        source: '/test2/:path*',
+        destination: '/:path*', // Redirect from /test2 to root
+        permanent: true, // Permanent redirect
+      },
+      {
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: 'www.sitfile.com',
+          },
+        ],
+        destination: 'https://sitfile.com', // Redirect www to non-www
+        permanent: true, // Permanent redirect
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
