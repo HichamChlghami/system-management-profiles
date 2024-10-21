40ec7cf0498121891af4eaed9e3bcb90e747228f
Hicham <121503241+HichamChlghami@users.noreply.github.com>
2024-10-21 16:34:32 +0100

    Update next.config.js  last update

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/test2/:path*',
-          destination: '/:path*', // Redirect to the same path without /test2
-          permanent: true, // This will send a 308 status code
-        },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  async redirects() {
+    return [
+      {
+        source: '/test2/:path*', // Match any path under /test2
+        destination: '/:path*', // Redirect to the same path without /test2
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
+
 
 
 
