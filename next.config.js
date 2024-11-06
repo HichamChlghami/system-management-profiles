3e4ba8af9cff175bee22bf8471a37800543f5951
Hicham <121503241+HichamChlghami@users.noreply.github.com>
2024-11-06 08:55:11 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/test2/:path*', // Match any path under /test2
+        source: '/system/:path*', // Match any path under /test2
         destination: '/:path*', // Redirect to the same path without /test2
         permanent: true, // 301 redirect
       },
